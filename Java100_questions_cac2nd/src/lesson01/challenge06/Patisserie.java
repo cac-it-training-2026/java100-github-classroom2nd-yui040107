f1453ea5a1c28239623ac71204e85e8111b75e05
yuioha <yuioha@C010423033.zipangu.cac.co.jp>
2026-04-15 15:10:40 +0900

    1-1

--- Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
@@ -21,6 +21,58 @@
 
 package lesson01.challenge06;
 
-public class Patisserie {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Patisserie {
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("[ポエール・ネルメ]");
+		System.out.println("ただいまより開店です!");
+
+		System.out.println("本日のおすすめ商品です。");
+
+		int citoron = 30;
+		int syokora = 30;
+		int pisutasyu = 30;
+
+		System.out.println("シトロン  \\250…残り" + citoron + "個");
+		System.out.println("ショコラ  \\280…残り" + syokora + "個");
+		System.out.println("ピスターシュ  \\320…残り" + pisutasyu + "個");
+
+		System.out.println("それぞれ何個ずつ買いますか？　（最大30個まで）");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("シトロン　　>");
+		String citoronbuy = reader.readLine();
+		int citoronbuycount = Integer.parseInt(citoronbuy);
+
+		System.out.println("ショコラ　　>");
+		String syokorabuy = reader.readLine();
+		int syokorabuycount = Integer.parseInt(syokorabuy);
+
+		System.out.println("ピスターシュ　　＞");
+		String pisutasyubuy = reader.readLine();
+		int pisutasyubuycount = Integer.parseInt(pisutasyubuy);
+
+		System.out.println("シトロン" + citoronbuycount + "個");
+		System.out.println("ショコラ" + syokorabuycount + "個");
+		System.out.println("ピスターシュ" + pisutasyubuycount + "個");
+
+		System.out.println("合計個数  " + (citoronbuycount + syokorabuycount + pisutasyubuycount) + "個");
+		System.out.println("合計金額　"
+				+ ((citoronbuycount * 250) + (syokorabuycount * 280) + (pisutasyubuycount * 320) + "円"));
+
+		System.out.println("をお買い上げですね。");
+		System.out.println("承りました。");
+
+		System.out.println("本日のおすすめ商品です。");
+
+		System.out.println("シトロン  \\250…残り" + (citoron - citoronbuycount) + "個");
+		System.out.println("ショコラ  \\280…残り" + (syokora - syokorabuycount) + "個");
+		System.out.println("ピスターシュ  \\320…残り" + (pisutasyu - pisutasyubuycount) + "個");
+
+	}
 }
