14c9dee3de7ea78d68e01f34b49c921798c47a55
yuioha <yuioha@C010423033.zipangu.cac.co.jp>
2026-04-20 16:25:16 +0900

    lesson1 10まで完了

---
+++ Java100_questions_cac2nd/src/lesson04/challenge06/Advertisement.java
@@ -0,0 +1,17 @@
+package lesson04.challenge06;
+
+public class Advertisement {
+
+	public static void main(String[] args) {
+		System.out.println("★マカロン屋の新規店員募集します！★");
+		System.out.println("・勤務時間");
+		System.out.println("AM８：００～PM２０：００");
+		System.out.println("・時給");
+		System.out.println("１３００円　土日祝：１３５０円");
+		System.out.println("・待遇");
+		System.out.println("交通費支給　月１万円まで");
+		System.out.println("賄い支給（マカロン）");
+
+	}
+
+}
